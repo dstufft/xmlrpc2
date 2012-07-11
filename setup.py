ea5b714b4c561c35c92c0094f5d3488a2df2bc67
Donald Stufft <donald.stufft@gmail.com>
2012-07-11 01:45:54 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+install_requires = [
+    "requests",
+]
+
+setup(
+    name="xmlrpc2",
+    version="0.1dev",
+
+    description="A port of xmlrpc from Python3 to Python 2.x using the Requests library",
+    long_description=open("README.rst").read(),
+    url="https://github.com/dstufft/xmlrpc2",
+    #license=open("LICENSE").read(),
+
+    author="Donald Stufft",
+    author_email="donald.stufft@gmail.com",
+
+    install_requires=install_requires,
+
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+)
