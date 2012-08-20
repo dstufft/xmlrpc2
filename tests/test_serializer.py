ba65d93a247be0325fbe2637178370b87e5614c7
Donald Stufft <donald.stufft@gmail.com>
2012-08-20 06:52:13 -0400

    condense some tests and test deserialization

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -11,57 +11,47 @@ from xmlrpc2.serializer import Serializer
 from lxml import etree
 
 
-def test_empty_method_call():
-    s = Serializer()
-    serialized = s.serialize({
-        "methodCall": {
-            "methodName": "test"
-        }
-    })
-    assert serialized == b"<methodCall><methodName>test</methodName></methodCall>"
-
-
-def test_empty_params_method_call():
-    s = Serializer()
-    serialized = s.serialize({
-        "methodCall": {
-            "methodName": "test",
-            "params": [],
-        }
-    })
-    assert serialized == b"<methodCall><methodName>test</methodName><params/></methodCall>"
-
-
-def test_all_data_types_method_call():
-    s = Serializer()
-    serialized = s.serialize({
-        "methodCall": {
-            "methodName": "test",
-            "params": [
-                True,
-                False,
-                1,
-                0,
-                -1,
-                10.5,
-                datetime.datetime(year=2012, month=8, day=20, hour=5, minute=32, second=5),
-                b"binary data!",
-                {
-                    "wat": "ok",
-                    "winful": True,
-                },
-                [
-                    "one",
-                    "two",
-                    "three",
-                    "and",
-                    "a",
-                    "four",
+@pytest.mark.parametrize(("inp", "expected"), [
+    ({"methodCall": {"methodName": "test"}}, b"<methodCall><methodName>test</methodName></methodCall>"),
+    ({"methodCall": {"methodName": "test", "params": []}}, b"<methodCall><methodName>test</methodName><params/></methodCall>"),
+    (
+        {
+            "methodCall": {
+                "methodName": "test",
+                "params": [
+                    True,
+                    False,
+                    1,
+                    0,
+                    -1,
+                    10.5,
+                    datetime.datetime(year=2012, month=8, day=20, hour=5, minute=32, second=5),
+                    b"binary data!",
+                    {
+                        "wat": "ok",
+                        "winful": True,
+                    },
+                    [
+                        "one",
+                        "two",
+                        "three",
+                        "and",
+                        "a",
+                        "four",
+                    ],
                 ],
-            ],
-        }
-    })
-    assert serialized == b"<methodCall><methodName>test</methodName><params><param><value><boolean>1</boolean></value></param><param><value><boolean>0</boolean></value></param><param><value><int>1</int></value></param><param><value><int>0</int></value></param><param><value><int>-1</int></value></param><param><value><double>10.5</double></value></param><param><value><dateTime.iso8601>2012-08-20T05:32:05</dateTime.iso8601></value></param><param><value><base64>YmluYXJ5IGRhdGEh</base64></value></param><param><value><struct><member><name>wat</name><value><string>ok</string></value></member><member><name>winful</name><value><boolean>1</boolean></value></member></struct></value></param><param><value><array><data><value><string>one</string></value><value><string>two</string></value><value><string>three</string></value><value><string>and</string></value><value><string>a</string></value><value><string>four</string></value></data></array></value></param></params></methodCall>"
+            }
+        },
+        b"<methodCall><methodName>test</methodName><params><param><value><boolean>1</boolean></value></param><param><value><boolean>0</boolean></value></param><param><value><int>1</int></value></param><param><value><int>0</int></value></param><param><value><int>-1</int></value></param><param><value><double>10.5</double></value></param><param><value><dateTime.iso8601>2012-08-20T05:32:05</dateTime.iso8601></value></param><param><value><base64>YmluYXJ5IGRhdGEh</base64></value></param><param><value><struct><member><name>wat</name><value><string>ok</string></value></member><member><name>winful</name><value><boolean>1</boolean></value></member></struct></value></param><param><value><array><data><value><string>one</string></value><value><string>two</string></value><value><string>three</string></value><value><string>and</string></value><value><string>a</string></value><value><string>four</string></value></data></array></value></param></params></methodCall>"
+    ),
+])
+def test_serialize_deserialize(inp, expected):
+    s = Serializer()
+    serialized = s.serialize(inp)
+    deserialized = s.deserialize(serialized)
+
+    assert serialized == expected
+    assert deserialized == inp
 
 
 @pytest.mark.parametrize(("inp", "expected"), [
